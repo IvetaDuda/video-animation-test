cf9d0e5afcb8d49ffaa30f31c792bb342ebeab8c
IvetaDuda <92565279+IvetaDuda@users.noreply.github.com>
2024-07-28 22:07:13 +0300

    add filles

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import About from '@/section/About/About';
+import Hero from '@/section/Hero/Hero';
+import styles from './page.module.css';
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <About />
+    </>
+  );
+}
